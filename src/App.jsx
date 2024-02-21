33ee99b08bcfad040968c26ac61ab8246c80bed2
Dário Souza <drio.srf@gmail.com>
2024-02-21 02:57:21 +0000

    Colocando overlay ao clicar em uma data no calendário

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css';
+import { useState } from "react";
 import Calendar from './components/Calendar';
+import Overlay from "./components/Overlay";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOverlay = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className='App'>
-        <a>Saudações William!</a>
+        <a>Saudações, William!</a>
+        <button onClick={toggleOverlay}>Open Overlay</button>
+        <Overlay isOpen={isOpen} onClose={toggleOverlay}>
+          <h1>Content in Overlay</h1>
+          </Overlay>
         <Calendar />
       </div>
     </>
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,39 +1,22 @@
 import React from 'react'
-import dayjs from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import Overlay from "./Overlay";
+import { useState } from "react";
 
-const Cal = () => {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer
-                components={[
-                'DatePicker',
-                'MobileDatePicker',
-                'DesktopDatePicker',
-                'StaticDatePicker',
-                ]}
-            >
-                <DemoItem label="Desktop variant">
-                <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
-                </DemoItem>
-                <DemoItem label="Mobile variant">
-                <MobileDatePicker defaultValue={dayjs('2022-04-17')} />
-                </DemoItem>
-                <DemoItem label="Responsive variant">
-                <DatePicker defaultValue={dayjs('2022-04-17')} />
-                </DemoItem>
-                <DemoItem label="Static variant">
-                <StaticDatePicker defaultValue={dayjs('2022-04-17')} />
-                </DemoItem>
-            </DemoContainer>
-        </LocalizationProvider>
-    )
+export default function StaticDatePickerLandscape() {
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false)
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDatePicker 
+        orientation="landscape"
+        onChange={() => setIsOverlayOpen(!isOverlayOpen)}
+      />
+      <Overlay isOpen={isOverlayOpen} onClose={() => setIsOverlayOpen(!isOverlayOpen)}>
+        <h1>Overlay</h1>
+      </Overlay>
+    </LocalizationProvider>
+  );
 }
-
-export default Cal
