5d3c8116b93374d8a94749ac47798d2ff28d12f1
gmachadom <120364460+gmachadom@users.noreply.github.com>
2024-02-21 05:53:47 +0000

    Overlays formatados (coloridos e bonitinhos)

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -9,11 +9,38 @@ export function Overlay({ isOpen, onClose, children }) {
           <div className="overlay__background" onClick={onClose} />
           <div className="overlay__container">
             <div className="overlay__controls">
-              <button
-                className="overlay__close"
-                type="button"
-                onClick={onClose}
-              />
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                
+                <button
+                  className="overlay__tarefa"
+                  type="button"
+                  style={{marginBottom: '10px'}}
+                />
+                <br/>
+                <button
+                  className="overlay__reuniao"
+                  type="button"
+                  style={{marginBottom: '10px'}}
+                />
+                <br/>
+                <button
+                  className="overlay__evento"
+                  type="button"
+                  style={{marginBottom: '10px'}}
+                />
+                <br/>
+                <button
+                  className="overlay__anotacao"
+                  type="button"
+                  style={{marginBottom: '10px'}}
+                />
+                <br/>
+                <button
+                  className="overlay__alarme"
+                  type="button"
+                  style={{marginBottom: '10px'}}
+                />
+              </div>
             </div>
             {children}
           </div>
